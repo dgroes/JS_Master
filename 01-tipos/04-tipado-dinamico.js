082f9b307aa363e545d1f7fc4447c18651ab74ab
dgroes <diegopasten78@gmail.com>
2024-03-28 17:35:11 -0300

    Propiedades del tipado dinamico.

---
+++ 01-tipos/04-tipado-dinamico.js
@@ -0,0 +1,15 @@
+// los lenguajes de tipado dinamico permite a las variables cambiar el tipo de dato por otro,
+// ejemplo; tengo la variable nombre que contiene "andres", y esta variable luego le puedo cambiar el dato por uno numerico
+
+let numero = 40;
+let nombre = "Hola Dieog";
+let verdadero = true;
+let undef;
+let nula = null;
+
+
+console.log(typeof nombre);
+console.log(typeof numero);
+console.log(typeof verdadero);
+console.log(typeof undef);
+console.log(typeof nula);
