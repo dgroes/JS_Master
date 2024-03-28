caabb62a1d4b7d1d092fa49e07a6db31657db8ac
dgroes <diegopasten78@gmail.com>
2024-03-28 17:33:59 -0300

    Valores primitivos.

---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,7 @@
+let numero = 1; /* numer */
+let texto = "Hola, que tal!"; //strings
+let verdadero = true; //boolean
+let falso = false; //boolean
+let noDefinido; //unefined
+let undef = undefined; //undefined
+let nulo = null; //null 
