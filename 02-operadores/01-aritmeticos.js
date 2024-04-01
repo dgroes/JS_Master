bc99c3b8f44e9ab372bdf59b8cbebeed6bdf079b
dgroes <diegopasten78@gmail.com>
2024-04-01 17:17:34 -0300

    operadores aritmetios.

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,21 @@
+let a = 5;
+let b = 7;
+
+console.log(a + b, 'Adición');
+console.log(a - b, 'Resta');
+console.log(a * b, 'Multiplicación');
+console.log(a / b, 'División');
+console.log(a % b, 'Modulo');
+console.log(a ** b, 'Potencia'); //potencia
+
+//Operadores de incrementar
+console.log(++a);
+
+console.log(a++);
+console.log(a);
+
+//OPeradores de decrementar.
+console.log(--b);
+
+console.log(b--);
+console.log(b);
