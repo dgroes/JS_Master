33f37eb04f007b87ffefe1839a69eab30a9af772
dgroes <diegopasten78@gmail.com>
2024-04-02 19:04:36 -0300

    Simbolos de comparación.

--- 02-operadores/03-comparacion.js
+++ 02-operadores/03-comparacion.js
@@ -1,2 +1,13 @@
 let a = 10;
+//Operadores relacionales
 console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+//Operadores de igualdad
+console.log(a == 10); //igual que 
+console.log(a != 10); //distinto de
+console.log(a == '10'); // el simbolo == pregunta si son iguales, ignorando el tipo de dato
+console.log(a === '10'); // con === comprueba si tiene el mismo tipo y valor
+console.log(a !== '10'); // disito tipo de dato o de valor se utiliza !==
