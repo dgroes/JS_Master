e966bf2881fb4784692c6f8aa57d4ca9565fbade
dgroes <diegopasten78@gmail.com>
2024-04-11 19:15:51 -0400

    For Of.

---
+++ 03-control-flujo/07-for-of.js
@@ -0,0 +1,19 @@
+//while
+//do while   
+//for
+// Con estos 3 lopps podemos calcular como vamos a realizar una iteración
+
+let animales = ['Mapache', 'León', 'Medusa'];
+
+console.log("For");
+for (let animal of animales){
+    console.log(animal);
+}
+
+console.log("while");
+let i = 0;
+while (i < animales.length){
+    console.log(animales[i])
+    i++;
+}
+
