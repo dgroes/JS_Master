5db40f2facf504a6bb31e4a6299fdb7afef2aec6
dgroes <diegopasten78@gmail.com>
2024-04-11 19:14:41 -0400

    While.

--- 03-control-flujo/03-while.js
+++ 03-control-flujo/03-while.js
@@ -3,6 +3,10 @@
 //Cuales son los números pares:
 let i = 0;
 while (i < 10) {
-    console.log(i);
-    i++
+    if (i % 2 == 0) {
+        console.log("Número par", i);
+    }
+    i++;
 }
+
+console.log("Fuera del while");
