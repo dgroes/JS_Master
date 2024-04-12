cbfc96bef5a129f232b96990d526ec7546a3ae54
dgroes <diegopasten78@gmail.com>
2024-04-12 19:04:44 -0400

    Switch

---
+++ 03-control-flujo/10-switch.js
@@ -0,0 +1,12 @@
+let accion = "Guardar";
+
+switch (accion) {
+    case 'Listar':
+        console.log("Acción de Listar");
+        break;
+    case 'Guardar':
+        console.log("Acción de guardar");
+        break;
+    default:
+        console.log("Acción no reconocida");
+}
