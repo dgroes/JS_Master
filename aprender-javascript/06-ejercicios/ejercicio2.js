30a2c543bbb0eb9840e2c3a5f15be0a4c0fcd691
Diego Pasten <diegopasten78@gmail.com>
2024-11-26 23:08:06 -0300

    ejercico 2

---
+++ aprender-javascript/06-ejercicios/ejercicio2.js
@@ -0,0 +1,10 @@
+/* Ejercicio 2
+
+Tenemos 7  cajas con 8 galletas rellenas de cocholate cada.Ejercicio
+¿Cuantas galletas en total tenemos en relaidad? */
+
+let boox = 7;
+let cookie = 8;
+
+let total = boox * cookie;
+console.log("El total de galltas es de: " + total);
