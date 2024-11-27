de6a36fc95cf0abf68b0f8d603ef17485cf97f9d
Diego Pasten <diegopasten78@gmail.com>
2024-11-26 23:08:21 -0300

    ejercicio 3

---
+++ aprender-javascript/06-ejercicios/ejercicio3.js
@@ -0,0 +1,11 @@
+/* Ejercico 3
+Cuantos minutos tienen 7 horas
+
+Utiliza variables y operadores para resolverlo
+
+*/
+
+let hora = 7; //Minutos
+const mimutosHora = 60;
+let total = hora * mimutosHora;
+console.warn("En 7 horas hay " + total + " Minutos");
