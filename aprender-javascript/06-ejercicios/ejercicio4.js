fe310e613e3a8b92085aabae8083a97baf77015d
Diego Pasten <diegopasten78@gmail.com>
2024-11-26 23:07:39 -0300

    ejerccio 4

---
+++ aprender-javascript/06-ejercicios/ejercicio4.js
@@ -0,0 +1,17 @@
+/* 
+    Ejercicio 4
+    Tenemos un jirafa en el zoo que pesa 1.120 kilos
+
+    y le damos de comer 141 kilos de hojas frecas.
+
+    Cuanto pesa ahora la jirafa?
+*/
+
+let jirafa = 1120; //Kilos
+let hojas = 141;
+
+let pesoTotal = jirafa + hojas;
+
+let resultado = `El peso total de la jirafa tras comer ${hojas} kilos, es de ${pesoTotal}.`;
+
+console.warn(resultado);
