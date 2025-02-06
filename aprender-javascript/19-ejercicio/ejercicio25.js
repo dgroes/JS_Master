85e566525316483bb86e63b464336ea04f55de68
Diego Pasten <diegopasten78@gmail.com>
2025-02-06 19:19:03 -0300

    feat: :egg: Se añade el ejercicio 25

---
+++ aprender-javascript/19-ejercicio/ejercicio25.js
@@ -0,0 +1,20 @@
+/* 
+    Ejercicio 25
+    Crea un array de ingredientes para una receta absurda
+    (Como pizza con chocolate y piña)
+
+    Muestra la receta completa en una frase divertida
+
+*/
+
+
+let ingredientes = [
+    ["Ingrediente", "Cantidad"],
+    ["Arróz", "200 Gramos"],
+    ["Ajo", "10 Gramos"],
+    ["Agua", "2 Litros"],
+    ["Chocolate", "400 gramos"],
+]
+
+
+document.write(ingredientes);
