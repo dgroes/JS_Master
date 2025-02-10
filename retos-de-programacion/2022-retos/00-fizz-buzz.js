f67332f97543d4deb09cd64cb7c8c25236841ae3
Diego Pasten <diegopasten78@gmail.com>
2025-02-10 20:23:51 -0300

    feat: :construction_worker: Reto Fizz Buzz

---
+++ retos-de-programacion/2022-retos/00-fizz-buzz.js
@@ -0,0 +1,27 @@
+/*
+ * Reto #0
+ * EL FAMOSO "FIZZ BUZZ"
+ * Fecha publicación enunciado: 27/12/21
+ * Fecha publicación resolución: 03/01/22
+ * Dificultad: FÁCIL
+ * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+
+let i = 1;
+while (i <= 50) {
+    if (((i % 3) == 0) && ((i % 5) == 0)) {
+        console.log("fizzbuzz");
+    } else if ((i % 3) == 0) {
+        console.log("fizz");
+    } else if ((i % 5) == 0) {
+        console.log("buzz");
+    } else {
+
+        console.log(i);
+    }
+    i++
+
+}
