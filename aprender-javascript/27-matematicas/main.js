4e2cea226ae84a2f699621967b4666768505667c
Diego Pasten <diegopasten78@gmail.com>
2025-02-11 20:17:53 -0300

    feat: :alien: Objetos Matematicos

---
+++ aprender-javascript/27-matematicas/main.js
@@ -0,0 +1,32 @@
+//Número PI
+console.log(Math.PI);
+
+//Número Euler
+console.log(Math.E);
+
+//Redondeo al entero más cercano
+console.log(Math.round(7.6));
+
+//Redondeo a la baja
+console.log(Math.floor(7.8));
+
+//Redondear al alza
+console.log(Math.ceil(7.000000001));
+
+//Nümeros al azar (del 0 al 100, con redondeo a la baja)
+console.log(Math.round(Math.random()) * 10);
+
+//VAlor absoluto
+console.log(Math.abs(-27.9));
+
+//Potencias (elevar)
+console.log(Math.pow(7, 8));
+
+//RAiz cuadrada + con 2 decilames con toFixed
+console.log(Math.sqrt(7).toFixed(2));
+
+//Dar el número maximo
+console.log(Math.max(12312,98745623,12352412,234134));
+
+//Dar el número minimo
+console.log(Math.min(12312,98745623,12352412,234134));
