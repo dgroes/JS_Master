16b03d328308f61fba8bd5a82215d5247164bd04
Diego Pasten <diegopasten78@gmail.com>
2025-02-14 20:21:39 -0300

    feat: :alien: fetch

--- aprender-javascript/30-fetch/main.js
+++ aprender-javascript/30-fetch/main.js
@@ -8,7 +8,7 @@ console.log(algo); // Recibirá una "Promesa" y estará "Pendiente"
 
 
 /* Para interactuar con el resultado */
-fetch(pokeApi)
+/* fetch(pokeApi)
     .then(red => red.json())
     .then(data => console.log(data));
 
@@ -19,11 +19,11 @@ function mostrarPokemon(){
         <header>${poke.name}</header>
     `
 }
-
-
+ */
+// Sacar datos de un backend de manera asincrona
 let idUser = 4;
 
-fetch("https://reqres.in/api/users/"+ idUser)
+fetch("https://reqres.in/api/users/" + idUser)
     .then(data => data.json())
     .then(usuario => {
         console.log(usuario.data);
@@ -40,3 +40,24 @@ function mostrarUsuario(user) {
         <img src="${user.avatar}" />
     `;
 }
+
+//Guardar Información
+const usuarioParaGuardar = {
+    name: "Diego Pastén",
+    job: "Desarrollador",
+};
+
+fetch("https://reqres.in/api/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(usuarioParaGuardar)
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
