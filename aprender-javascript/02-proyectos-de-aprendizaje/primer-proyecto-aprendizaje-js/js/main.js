f569b0c78aab6a44ecac56195c33c6dcbef3c724
Diego Pasten <diegopasten78@gmail.com>
2025-03-03 22:52:46 -0300

    feat: :zap: Main JS

---
+++ aprender-javascript/02-proyectos-de-aprendizaje/primer-proyecto-aprendizaje-js/js/main.js
@@ -0,0 +1,12 @@
+// Scroll Top
+window.addEventListener("DOMContentLoaded", () => {
+    let btnTop = document.querySelector(".footer__top");
+
+    btnTop.addEventListener("click", () => {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+        });
+    })
+})
