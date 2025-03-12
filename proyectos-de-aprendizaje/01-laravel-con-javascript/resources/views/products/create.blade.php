9396f9cd5bdf20831911fafa466f071e0096f6a9
Diego Pasten <diegopasten78@gmail.com>
2025-03-11 23:49:37 -0300

    feat: :art: View de creación de producto

--- proyectos-de-aprendizaje/01-laravel-con-javascript/resources/views/products/create.blade.php
+++ proyectos-de-aprendizaje/01-laravel-con-javascript/resources/views/products/create.blade.php
@@ -1,3 +1,45 @@
-<h1>
-    Formulario para agregar productos.
-</h1>
+@extends('layouts.pro') {{-- C01: Extends --}}
+
+@section('title', 'Crear producto')
+
+@section('content')
+    <h1>Creación de producto</h1>
+
+    <section>
+        <form>
+            <fieldset>
+
+                {{-- Nombre del producto --}}
+                <label>
+                    Nombre del producto
+                    <input type="text" name="name" id="name" placeholder="Producto"/>
+                </label>
+
+                {{-- Descripción del producto --}}
+                <label>
+                    Descripción del producto
+                    <input type="text" name="description" id="description" placeholder="Descripción"/>
+                </label>
+
+                {{-- Precio del producto --}}
+                <label>Precio del producto
+                    <input type="number" name="price" id="price" placeholder="Precio"/>
+                </label>
+
+                {{-- Stock de producto --}}
+                <label>Stock del producto
+                    <input type="number" name="stock" id="stock" placeholder="Stock"/>
+                </label>
+
+                {{-- Imagen del producto --}}
+                <label>Imagen del producto
+                <input type="file" name="image" id="image">
+                </label>
+
+            </fieldset>
+
+            <input type="submit" value="Crear Producto" />
+        </form>
+    </section>
+
+@endsection
