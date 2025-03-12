163f5d1b2c62ccffef789897c57e2312a1259c8a
Diego Pasten <diegopasten78@gmail.com>
2025-03-11 23:52:10 -0300

    feat: :card_file_box: Modelos de Product, Role, Sale, SaleDetail

--- proyectos-de-aprendizaje/01-laravel-con-javascript/app/Models/Product.php
+++ proyectos-de-aprendizaje/01-laravel-con-javascript/app/Models/Product.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    //Rellenado de campos permitidos
+    protected $fillable = ['name', 'description', 'price', 'stock', 'image'];
+
+
 }
