614726ff43a1c9113c7173ea22f634e63353510d
Diego Pasten <diegopasten78@gmail.com>
2025-03-20 01:39:44 -0300

    feat: :building_construction: Se modifica la tabla y el layout

--- proyectos-de-aprendizaje/01-laravel-con-javascript/resources/views/layouts/pro.blade.php
+++ proyectos-de-aprendizaje/01-laravel-con-javascript/resources/views/layouts/pro.blade.php
@@ -1,14 +1,20 @@
 <!doctype html> {{-- C00: Layaouts como Herencia de Plantillas --}}
-<html lang="es">
+<html lang="es" data-theme="light">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="color-scheme" content="light dark">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"> --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.yellow.min.css">
 
     <title>@yield('title', 'Mi Aplicación')</title> {{-- C02: Yield con Section --}}
+
+    {{-- C08: JS con Laravel --}}
+    {{-- C07: Carga de CSS personalizado con Vite --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 
 <body>
@@ -18,13 +24,24 @@
         {{-- Header --}}
         <nav>
             <ul>
-                <li><strong>Mi Tiendap</strong></li>
+                <li><strong>Mi Tienda <x-icons.heart /> </strong></li>
+
             </ul>
             <ul>
                 <li><a href="{{ route('products.index') }}">Productos</a></li>
                 <li><a href="{{ route('products.create') }}">Nuevo Producto</a></li>
+                <li id="theme-toggle" class="cursor-pointer">
+                    <x-icons.sun id="light" />
+                    <x-icons.moon id="dark" />
+                </li>
+
+
+
             </ul>
+
+
         </nav>
+
 
         {{-- Contenido principal de productos --}}
         @yield('content') {{-- Aquí se insertará el contenido de cada vista --}}
--- proyectos-de-aprendizaje/01-laravel-con-javascript/resources/views/products/index.blade.php
+++ proyectos-de-aprendizaje/01-laravel-con-javascript/resources/views/products/index.blade.php
@@ -3,24 +3,48 @@
 @section('title', 'Lista de Productos')
 
 @section('content') {{-- C02: Yield con Section --}}
-    <h2>Lista de Productos</h2>
-    <ul>
-        @if (count($products) > 0)
-            @foreach ($products as $product)
-                <li>
-                    <a href="{{ route('products.show', $product->id) }}">{{ $product->name }}</a> - ${{ $product->price }}
-                </li>
-                {{-- C06: asset sin public --}}
-                <li>
-                    <img src="{{ asset('images/' . $product->image) }}" alt="{{ $product->name }}" style="width: 100px;">
-                </li>
+    <h2>Lista de Productos
+    </h2>
+    <table  class="striped">
+        <thead data-theme="light">
+            <tr>
+                <th scope="col">Producto </th>
+                <th scope="col">Descripción</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Stock</th>
+                <th scope="col">Imagen</th>
+                <th scope="col">Acción</th>
+            </tr>
+        </thead>
+        <tbody>
+            @if (count($products) > 0)
+                @foreach ($products as $product)
+                    <tr>
+                        <td><a href="{{ route('products.show', $product->id) }}">{{ $product->name }}</a></td>
+                        <td>{{ $product->description }}</td>
+                        <td>${{ $product->price }}</td>
+                        <td>{{ $product->stock }}</td>
+                        <td>
+                            <img src="{{ asset('images/' . $product->image) }}" alt="{{ $product->name }}"
+                                style="width: 100px;">
+                        </td>
+                        <td>
+                            <a href="{{ route('products.edit', $product->id) }}">✏️</a> |
+                            <form action="{{ route('products.destroy', $product->id) }}" method="POST"
+                                style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" style="background:none; border:none; cursor:pointer;">🗑️</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            @else
+                <tr>
+                    <td colspan="6">No hay productos de momento!</td>
+                </tr>
+            @endif
+        </tbody>
+    </table>
 
-            @endforeach
-
-        @else
-            <article>No hay productos de momento!</article>
-
-        @endif
-
-    </ul>
 @endsection
